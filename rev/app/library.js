3a2b5af2f1fa75a8ee45d5ff7948d9b528bf9957
Nosisky <nosisky@gmail.com>
2017-03-14 11:48:58 +0100

    I added a new function to reverse the given string

---
+++ rev/app/library.js
@@ -0,0 +1,11 @@
+'use strict'
+const pally = {
+	reverseString(str) {
+		if(typeof str !== "string") {
+			const msg = "Invalid Input, Expecting a string!"
+			return msg;
+		}
+		
+}
+}
+module.exports = pally;
